f4260052a09986d3a435bf83a6417eac48c93d81
Samuel Taber <admin@cadianetwork.com>
2019-03-19 14:53:19 -0400

    Update TeamName.java

--- src/com/tabergames/util/TeamName.java
+++ src/com/tabergames/util/TeamName.java
@@ -6,6 +6,8 @@ public class TeamName {
 
 	public static String teamSecurity = ChatColor.BLUE + "Security";
 	public static String teamRobbers = ChatColor.RED + "Robbers";
+	public static String securityDepend = "Security";
+	public static String robbersDepend = "Robbers";
 	
 	
 }
