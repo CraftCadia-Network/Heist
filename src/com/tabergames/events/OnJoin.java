64a463f6f30fdae99664cffb04111010a0d33032
Samuel Taber <admin@cadianetwork.com>
2019-03-19 10:50:14 -0400

    Create OnJoin.java

---
+++ src/com/tabergames/events/OnJoin.java
@@ -0,0 +1,20 @@
+
+package com.tabergames.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import com.tabergames.util.PlayerHandler;
+
+public class OnJoin implements Listener{
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		
+		Player p = e.getPlayer();
+		PlayerHandler.SetupPlayer(p);
+	}
+	
+}
